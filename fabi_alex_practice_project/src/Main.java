168deca20fec3e300d054adc66b1e9e2762e8a9e
Alexander-Rus <alex.rus10@gmail.com>
2022-12-22 15:00:14 -0500

    add readme file

--- fabi_alex_practice_project/src/Main.java
+++ fabi_alex_practice_project/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Alex's world!");
         System.out.println("This is a change that I made here!");
-        /This is a comment on this branch
+        //This is a comment on this branch
         System.out.println("Alex branch@");
     }
 }
